1cc5e5bceb55e3c07488e909e4470f8c329f06ac
erojasfredini <erojasfredini@gmail.com>
2020-09-17 22:05:44 -0300

    - Iniciamos nuestro proyecto :)

---
+++ Assets/Scripts/CamaraPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraPlayer : MonoBehaviour
+{
+    public Transform objetivo;
+    private Vector3 desplazamiento;
+    void Start()
+    {
+        desplazamiento = transform.position - objetivo.position;
+    }
+
+    public float velocidad = 1.0f;
+
+    void Update()
+    {
+        Vector3 target = objetivo.position + desplazamiento;
+        transform.position = Vector3.Lerp(transform.position, target, Time.deltaTime * velocidad);
+    }
+}
---
+++ Assets/Scripts/MovimientoPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoPlayer : MonoBehaviour
+{
+    public float velocidad;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private float h;
+    private float v;
+
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = (h * Vector3.right + v * Vector3.forward).normalized * velocidad;
+    }
+}
